2038cdb3c64dfdffa7af4233c4795e9602a6b341
Mathew Puryear <mathewpuryear@gmail.com>
2023-04-18 00:30:02 -0700

    Add gold currency

---
+++ Assets/Scripts/TemporaryLearning/GoldVacuum.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GoldVacuum : MonoBehaviour
+{
+    private string GOLD_TAG = "Gold";
+    public AudioClip coinSound;
+    public CurrencyView currencyView;
+    private AudioSource audioSouce;
+
+    void Start() 
+    {
+        audioSouce = GetComponent<AudioSource>();
+        audioSouce.clip = coinSound;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag != GOLD_TAG) { return; }
+
+        currencyView.OnPickupGold(1);
+        PlayGoldSound();
+        Destroy(col.gameObject);
+
+    }
+
+    void PlayGoldSound()
+    {
+        if (audioSouce.isPlaying) { return; }
+        audioSouce.Play();
+    }
+}
---
+++ Assets/Scripts/TemporaryLearning/OnDeathSpawnPrefab.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HealthManager))]
+public class OnDeathSpawnPrefab : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    private HealthManager healthManager;
+    public float percentSpawnChance = 30f;
+
+    void Start() 
+    {
+        healthManager = GetComponent<HealthManager>();
+        healthManager.OnDeath += OnDeath;
+    }
+
+    void OnDestroy()
+    {
+        healthManager.OnDeath -= OnDeath;
+    }
+
+    void OnDeath() 
+    {
+        if(Random.Range(0f, 100f) <= percentSpawnChance)
+        {
+            Instantiate(prefabToSpawn, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+    }
+}
